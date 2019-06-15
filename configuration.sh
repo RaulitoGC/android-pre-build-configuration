388ef8d37a5f921ce967bcc1d3c88d8d332ce4f4
raul <rguzmanc.161@gmail.com>
2019-06-15 00:05:10 -0500

    feat: update configuration file

--- configuration.sh
+++ configuration.sh
@@ -1,26 +1,34 @@
 # !/bin/bash
 
-#use JSON
-
-
-# function jsonval {
-#     temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
-#     echo ${temp##*|}
-# }
+colorPrimaryKey=""
 
 response=`curl -s -d "clientId=client-id-abcd" -X POST https://private-a014d-singlelogin.apiary-mock.com/client/configuration`
 
-# pos=`echo $(response) | awk match($1,"colorPrimary")`
-# echo $pos
-# temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop  | cut -d":" -f2| sed -e 's/^ *//g' -e 's/ *$//g' `
-# echo ${temp##*|}
+IFS=','
+array=( $response )
 
-ls
-pwd
-
-# prop='colorPrimary'
-# picurl=`jsonval`
-
-# `curl -s -X POST $picurl`
-
+for value in "${array[@]}"
+do
+	#echo $value
+	case $value in
+	   *colorPrimaryDark*)
+	      	temp=${value#*:}
+	      	colorPrimaryDark="${temp//\"}"
+	      	;;
+	   *colorPrimary*)
+	      	temp=${value#*:}
+	      	colorPrimary="${temp//\"}"
+	      	;;
+	   *colorAccent*)
+	      	temp=${value#*:}
+	      	colorAccent="${temp//\"}"
+	      	;;
+		*logo*)
+	      	logo=${value#*:}
+	      	;;
+	   *)
+	 		echo "$value is unknown"
+	     	;;
+	esac
+done
 
