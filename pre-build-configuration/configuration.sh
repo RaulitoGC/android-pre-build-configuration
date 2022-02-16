7cc7f7f521fea6dcaf9bdd7cfed5f0341c6c1ec7
Raul Guzman <rguzmanc.161@gmail.com>
2022-02-16 17:53:10 -0500

    Feature: Adding comments to the script

--- pre-build-configuration/configuration.sh
+++ pre-build-configuration/configuration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Function that gets back the value of the given key from JSON
+# Color keys from color-format.xml file
 COLOR_PRIMARY_KEY="COLOR_PRIMARY_KEY"
 COLOR_PRIMARY_DARK_KEY="COLOR_PRIMARY_DARK_KEY"
 COLOR_ACCENT_KEY="COLOR_ACCENT_KEY"
@@ -12,18 +12,20 @@ LOCAL_COLOR_RESOURCE_PATH='../app/src/main/res/values/'
 URL='https://gist.githubusercontent.com/RaulitoGC/dba916a37a0e2299ce0b2af16692881e/raw/a69c30bb23777b861a762f431975864ecd60153e/android-color-configuration.json'
 RESPONSE=$(curl "$URL")
 
+# Creating output folder, copy the color file format and rename it
 mkdir -p output
 cp 'colors-format.xml' 'output/'
 mv output/colors-format.xml output/colors.xml
 
+# Parsing the json from network to our colors (colorPrimary, colorPrimaryDark and colorAccent)
 COLOR_PRIMARY=$(echo "$RESPONSE" | grep -o '"colorPrimary": "[^"]*' | grep -o '[^"]*$')
 COLOR_PRIMARY_DARK=$(echo "$RESPONSE" | grep -o '"colorPrimaryDark": "[^"]*' | grep -o '[^"]*$')
 COLOR_ACCENT=$(echo "$RESPONSE" | grep -o '"colorAccent": "[^"]*' | grep -o '[^"]*$')
 
-#Replace values from network
+#Replace values from network to our local file using keys
 sed -i '' -e "s/$COLOR_PRIMARY_KEY/$COLOR_PRIMARY/" output/colors.xml
 sed -i '' -e "s/$COLOR_PRIMARY_DARK_KEY/$COLOR_PRIMARY_DARK/" output/colors.xml
 sed -i '' -e "s/$COLOR_ACCENT_KEY/$COLOR_ACCENT/" output/colors.xml
 
-# Move and replace the current local colors file for the remote one
+# Move and replace the current local colors file
 cp -f output/colors.xml $LOCAL_COLOR_RESOURCE_PATH
